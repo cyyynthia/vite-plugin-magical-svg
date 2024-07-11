7a11ca80b657ff2ac081b60d46eff03079c630d4
Mary <git@mary.my.id>
2024-07-11 13:42:45 +0700

    refactor: revert to document.createElement for dom target

--- runtime/dom.js
+++ runtime/dom.js
@@ -1,16 +1,15 @@
-const template = (html) => {
-	const tmpl = document.createElement("template");
-	tmpl.innerHTML = html;
-
-	return tmpl.content.firstChild;
-};
-
 export const createSvg = (viewBox, symbol) => {
-	const tmpl = template(`<svg viewBox="${viewBox}"><use href="${symbol}>`);
-	return () => tmpl.cloneNode(true);
+	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+	svg.setAttribute("viewBox", viewBox);
+	use.setAttribute("href", symbol);
+	svg.appendChild(use);
+	return svg;
 };
 
 export const createSvgDEV = (viewBox, xml) => {
-	const tmpl = template(`<svg viewBox="${viewBox}">${xml}`);
-	return () => tmpl.cloneNode(true);
+	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	svg.setAttribute("viewBox", viewBox);
+	svg.innerHTML = xml;
+	return svg;
 };
