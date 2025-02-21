40b0866f2bf6437169c4a292920498b72ded2046
mary <148872143+mary-ext@users.noreply.github.com>
2025-02-21 21:02:51 +0700

    feat: mark Vue components as raw (#19)

--- runtime/vue.js
+++ runtime/vue.js
@@ -1,4 +1,4 @@
-import { createElementBlock, createElementVNode, openBlock } from "vue";
+import { createElementBlock, createElementVNode, markRaw, openBlock } from "vue";
 
 export var createSvg = /*@__NO_SIDE_EFFECTS__*/ (viewBox, width, height, symbol) => {
 	// <svg viewBox="{{viewBox}}" width="{{width}}" height="{{height}}"><use href="{{symbol}}" /></svg>
@@ -6,20 +6,20 @@ export var createSvg = /*@__NO_SIDE_EFFECTS__*/ (viewBox, width, height, symbol)
 	let _hoisted_2 = createElementVNode("use", { href: symbol }, null, -1);
 	let _hoisted_3 = [_hoisted_2];
 
-	return {
+	return markRaw({
 		render: (_ctx, _cache) => {
 			return openBlock(), createElementBlock("svg", _hoisted_1, _hoisted_3);
 		},
-	};
+	});
 };
 
 export var createSvgDEV = /*@__NO_SIDE_EFFECTS__*/ (viewBox, width, height, xml) => {
 	// <svg viewBox="{{viewBox}}" width="{{width}}" height="{{height}}" innerHTML="{{xml}}"></svg>
 	let _hoisted_1 = { viewBox, width, height, innerHTML: xml };
 
-	return {
+	return markRaw({
 		render: (_ctx, _cache) => {
 			return openBlock(), createElementBlock("svg", _hoisted_1);
 		},
-	};
+	});
 };
