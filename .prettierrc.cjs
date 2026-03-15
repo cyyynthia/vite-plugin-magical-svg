36c56c9afa2f6b5a22fc0092b85204ff187a2d73
Liam Potter <liam@liampotter.co.uk>
2026-03-15 23:21:12 +0000

    add basic testing

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 120,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	arrowParens: 'always',
+	endOfLine: 'lf'
+}
