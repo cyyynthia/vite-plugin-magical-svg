50581a637f4f791812ef5722c45e87b1a69f8cf5
Liam Potter <liam@liampotter.co.uk>
2026-03-27 16:28:48 +0000

    - let vitest output it's own version - output OS info

--- test/setup.ts
+++ test/setup.ts
@@ -1,14 +1,11 @@
-import { createRequire } from 'node:module'
 import { version as viteVersion } from 'vite'
 
-const require = createRequire(import.meta.url)
-const { version: vitestVersion } = require('vitest/package.json') as { version: string }
 
 export function setup() {
 	const info = [
+		`OS ${process.platform} ${process.arch}`,
 		`Node ${process.version}`,
 		`Vite ${viteVersion}`,
-		`Vitest ${vitestVersion}`,
 	]
 
 	console.log(`\n  ${info.join(' / ')}\n`)
